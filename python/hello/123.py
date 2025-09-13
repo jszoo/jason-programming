963699ed2d8688e6ce8263e90cb4b081ee9960f0
jszoo <jszoo@outlook.com>
2025-09-13 18:30:29 +0800

    include perlin

--- python/hello/123.py
+++ python/hello/123.py
@@ -1,7 +1,9 @@
 from ursina import *
-from ursina.prefabs.first_person_controller import FirstPersonController    
+from ursina.prefabs.first_person_controller import FirstPersonController   
 
+from perlin_noise import PerlinNoise
 class Block(Button):
+
     def __init__(self,position=(0,0,0)):
         super().__init__(
             parent = scene,
@@ -9,8 +11,8 @@ class Block(Button):
             model = 'cube',
             orgin_y = 0.5,
             texture = 'white_cube',
-            color = color.white,
-            highlight_color = color.green,
+            color = color.green,
+            highlight_color = color.white,
         )
 
     def input(self,key):
@@ -19,12 +21,23 @@ class Block(Button):
                 block = Block(position = self.position + mouse.normal)
             if key == 'right mouse down':
                 destroy(self)
-    
-app = Ursina()
 
-for z in range(8):
-    for x in range(8):
+app = Ursina()
+scale = 24
+
+noise = PerlinNoise(octaves=3,seed=2023)
+for z in range(30):
+    for x in range(30):
         block = Block(position = (x,0,z))
+        block.y = floor(noise([x/scale,z/scale])*8)
+
+scene.fog_color = color.white
+scene.fog_density = 0.04
+
+def input(key):
+    if key == 'q' or key =='escape':
+        quit()
+        application.quit()
 
 player = FirstPersonController()
 
